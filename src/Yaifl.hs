fc87a3f198d46316b2067cb074b1bbfa0cd7d2f9
Avery <thecommunistduck@hotmail.co.uk>
2020-07-05 15:18:11 +0100

    Created

---
+++ src/Yaifl.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2020 Avery
+SPDX-License-Identifier: MIT
+Maintainer: Avery <thecommunistduck@hotmail.co.uk>
+
+Yet another interactive fiction library.
+-}
+
+module Yaifl
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
