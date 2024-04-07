8e74ba216ee2ddd21ba338f20ff04fc43abf3764
Avery <thecommunistduck@hotmail.co.uk>
2024-04-07 18:23:08 +0200

    Make it work with GHC 9.8.2 (sike: it already did)

--- shell.nix
+++ shell.nix
@@ -6,13 +6,13 @@ in
   pkgs.mkShell {
     buildInputs = with pkgs; [
       cabal-install
-      haskell.compiler.ghc948
+      haskell.compiler.ghc98
       binutils
       delta
       zlib
       zlib.dev
       zlib.out
-      haskell.packages.ghc94.haskell-language-server
+      haskell.packages.ghc98.haskell-language-server
     ];
     shellHook = ''
       eval $(egrep ^export ${unstable.ghc}/bin/ghc)
